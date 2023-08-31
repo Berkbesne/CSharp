5bd7238c426e4907fc9a9cfa412734755ac8ac35
Tuncer Berk Besne <77729477+Berkbesne@users.noreply.github.com>
2023-09-01 02:07:27 +0300

    Algorithm_Question

---
+++ Algorithm Questions/Algorithn_Q3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bir sayı giriniz");
+int a=Convert.ToInt32(Console.ReadLine());
+string[] strings=new string[a];
+for (int i = 0; i < strings.Length ; i++)
+{
+    strings[i] = Console.ReadLine();
+
+}
+Array.Reverse(strings);
+foreach (string str in strings)
+{
+    Console.Write(""+str);
+}
+  
