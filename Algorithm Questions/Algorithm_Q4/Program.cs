20d2663778c542e4a8bf6949d75bff5a2e392456
Tuncer Berk Besne <77729477+Berkbesne@users.noreply.github.com>
2023-09-01 02:12:03 +0300

    Algorithm_Question

---
+++ Algorithm Questions/Algorithm_Q4/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Bir cümle giriniz:");
+string cumle = Console.ReadLine();
+
+int kelimeSayisi = KelimeSayisiHesapla(cumle);
+int harfSayisi = HarfSayisiHesapla(cumle);
+
+Console.WriteLine($"Girilen cümledeki toplam kelime sayısı: {kelimeSayisi}");
+Console.WriteLine($"Girilen cümledeki toplam harf sayısı: {harfSayisi}");
+        
+
+        static int KelimeSayisiHesapla(string str)
+{
+    string[] kelimeler = str.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+    return kelimeler.Length;
+}
+
+static int HarfSayisiHesapla(string str)
+{
+    int harfSayisi = 0;
+    foreach (char karakter in str)
+    {
+        if (char.IsLetter(karakter))
+        {
+            harfSayisi++;
+        }
+    }
+    return harfSayisi;
+}
+    
