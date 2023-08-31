59f4499ea01cf1aaed98feb623dff46b0bc25a3d
Tuncer Berk Besne <77729477+Berkbesne@users.noreply.github.com>
2023-09-01 01:48:08 +0300

    Algorithm_Q1

---
+++ Algorithm Questions/Algorithm_Questions1/Program.cs
@@ -0,0 +1,27 @@
+﻿See https://aka.ms/new-console-template for more information
+Console.WriteLine("Pozitif Sayı Giriniz");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a > 0)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine("Sayı negatif");
+}
+Console.WriteLine("Sayi giriniz");
+int b = Convert.ToInt32(Console.ReadLine());
+int[] dizi = new int[b];
+for (int i = 0; i < dizi.Length; i++)
+{
+    dizi[i] = Convert.ToInt32(Console.ReadLine());
+
+
+}
+foreach (int num in dizi)
+{
+    if (num % 2 == 0)
+    {
+        Console.WriteLine(num);
+    }
+}
