511f2f5776a786361af4f4ed7be30d0455cf64cf
Tuncer Berk Besne <77729477+Berkbesne@users.noreply.github.com>
2023-09-01 01:59:02 +0300

    Algorithm_Q2

---
+++ Algorithm Questions/Algorithm_Q2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Sayı Giriniz");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Sayi giriniz");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] b = new int[n];
+for (int i = 0; i < b.Length-1; i++)
+{
+   b[i]=Convert.ToInt32(Console.ReadLine());
+   
+}
+foreach (int num in b)
+{
+    if (num == m || num % m == 0)
+    {
+        Console.Write(" "+num);
+    }
+
+}
