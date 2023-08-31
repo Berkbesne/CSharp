771cda9747a4daa36889749ce2f20b210a879220
Tuncer Berk Besne <77729477+Berkbesne@users.noreply.github.com>
2023-08-31 17:00:27 +0300

    Array Class Methods

---
+++ Array/Array_Class_Methods/Array_Class_Methods/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Array_Class_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 23, 24, 32, 19, 45, 56 };
+            Console.WriteLine("   Unordered Array  ");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine("  Sort Array  ");
+            Array.Sort(numbers);
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine("   Array Clear  ");
+            Array.Clear(numbers, 2, 3);
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            Array.Reverse(numbers);
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            ;
+
+            Console.WriteLine(Array.IndexOf(numbers, 56));
+          
+
+        }
+    }
+}
