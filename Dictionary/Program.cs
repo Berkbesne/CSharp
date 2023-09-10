8abeb753387d7fc2607ef5ac5db5819151b23243
Tuncer Berk Besne <77729477+Berkbesne@users.noreply.github.com>
2023-09-11 02:02:36 +0300

    Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,9 @@
+﻿Dictionary<int,string> Users=new Dictionary<int,string>();
+Users.Add(10,"Berk ");
+Users.Add(20, "Özgür ");
+Users.Add(30, "ALi");
+foreach (var item in Users)
+{
+    Console.WriteLine(item);    
+}
+Console.WriteLine(Users.Count);
