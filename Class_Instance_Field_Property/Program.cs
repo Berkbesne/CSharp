e9c4d0dd41d4b4fa80a9903e07224bda679b3879
Tuncer Berk Besne <77729477+Berkbesne@users.noreply.github.com>
2023-09-11 05:53:15 +0300

    Class

---
+++ Class_Instance_Field_Property/Program.cs
@@ -0,0 +1,21 @@
+﻿Worker woker1 = new Worker();
+woker1.Name = "Tuncer Berk";
+woker1.Surname = "Besne";
+woker1.No = 118;
+woker1.Description = "Salesman";
+woker1.WorkerInfo();
+Console.WriteLine("*******");
+
+class Worker{
+    public string Name;
+    public string Surname;
+    public int No;
+    public string Description;
+    public void WorkerInfo()
+    {
+        Console.WriteLine("Woker Name: {0}", Name);
+        Console.WriteLine("Worker Surname: {0}",Surname);
+        Console.WriteLine("Worker No:{0} ",No);
+        Console.WriteLine("Worker Description:{0}", Description);
+    }
+}
