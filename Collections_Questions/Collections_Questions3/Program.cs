a202f6cdb5d97df971464a7a347619e1f54496f8
Tuncer Berk Besne <77729477+Berkbesne@users.noreply.github.com>
2023-09-11 05:37:51 +0300

    Collections Questions 3

---
+++ Collections_Questions/Collections_Questions3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a sentence: ");
+        string sentence = Console.ReadLine();
+
+        List<char> vowelList = new List<char>();
+
+        foreach (char letter in sentence.ToLower())
+        {
+            if ("aeiou".Contains(letter))
+            {
+                vowelList.Add(letter);
+            }
+        }
+
+      
+        vowelList.Sort();
+
+        
+        Console.WriteLine("Sorted Vowels: ");
+        foreach (char vowel in vowelList)
+        {
+            Console.Write(vowel + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
