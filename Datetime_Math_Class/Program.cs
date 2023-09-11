15ea2cd48a786b662df3012c2cc3519473042a05
Tuncer Berk Besne <77729477+Berkbesne@users.noreply.github.com>
2023-09-11 04:12:24 +0300

    DateTime And Math

--- Datetime_Math_Class/Program.cs
+++ Datetime_Math_Class/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-Console.WriteLine(DateTime.Now); // günün tarihini getirir.
+Console.WriteLine(DateTime.Now);
 Console.WriteLine(DateTime.Now.Date);
 Console.WriteLine(DateTime.Now.Day);
 Console.WriteLine(DateTime.Now.Month);
@@ -29,9 +29,9 @@ Console.WriteLine(DateTime.Now.ToString("MMMM")); //April
 Console.WriteLine(DateTime.Now.ToString("yy")); //21
 Console.WriteLine(DateTime.Now.ToString("yyyy")); //2021
 
-Console.WriteLine("***** Math Kütüphanesi *****");
-//Math Kütüphanesi
-Console.WriteLine(Math.Abs(-25)); // 25;
+Console.WriteLine("***** Math  *****");
+
+Console.WriteLine(Math.Abs(-25)); 
 Console.WriteLine(Math.Sin(10));
 Console.WriteLine(Math.Cos(10));
 Console.WriteLine(Math.Tan(10));
@@ -41,8 +41,8 @@ Console.WriteLine(Math.Round(22.7)); // 23
 Console.WriteLine(Math.Floor(22.7)); // 22
 Console.WriteLine(Math.Max(2, 6));
 Console.WriteLine(Math.Min(2, 6));
-Console.WriteLine(Math.Pow(3, 4)); // 3^4 û verir. 81.
-Console.WriteLine(Math.Sqrt(9)); // karekök alır. 3
-Console.WriteLine(Math.Log(9)); // 9 un e tabanındaki logoritmik karşılığını getirir.
-Console.WriteLine(Math.Exp(3)); // e üzeri 3 û verir.
-Console.WriteLine(Math.Log10(10)); // 10 sayısının logaritma 10 tabanındaki karşılığı
+Console.WriteLine(Math.Pow(3, 4));
+Console.WriteLine(Math.Sqrt(9)); 
+Console.WriteLine(Math.Log(9));
+Console.WriteLine(Math.Exp(3));
+Console.WriteLine(Math.Log10(10)); 
