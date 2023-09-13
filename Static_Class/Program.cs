25aa36063524c7124a169c8948f2b4e169226f10
Tuncer Berk Besne <77729477+Berkbesne@users.noreply.github.com>
2023-09-13 09:28:22 +0300

    Static Class

---
+++ Static_Class/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Number of Worker : {0}", Worker.NumberEmployees);
+Worker worker = new Worker("Berk","Besne","IK");
+
+
+
+
+
+
+
+
+class Worker
+{
+
+    private static int numberEmployees;
+
+    public static int NumberEmployees { get => numberEmployees; }
+    private string Name;
+    private string Surname;
+    private string Department;
+    static Worker() 
+    {
+        numberEmployees = 0;
+    }
+    public Worker(string name,string surname,string  departman)
+    { 
+        this.Name = name;
+        this.Surname = surname;
+        this.Department = departman;
+        NumberEmployees++;
+    }
+}
