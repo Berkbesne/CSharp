df53f03abfdabf233223a49caf443cd8457a6b81
Tuncer Berk Besne <77729477+Berkbesne@users.noreply.github.com>
2023-09-18 06:39:36 +0300

    Enum

---
+++ Enum/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine(Days.Friday);
+Console.WriteLine((int)Days.Friday);
+
+int heat = 32;
+if (heat <= (int)Weather.Normal)
+{
+    Console.WriteLine("Let's wait for the weather to warm up to go out.");
+}
+else if (heat >= (int)Weather.Normal)
+{
+    Console.WriteLine("It's hot to go out");
+}
+else if (heat >= (int)Weather.Normal && heat < (int)Weather.SoHot)
+{
+    Console.WriteLine("Let's go out");
+}
+
+
+
+
+enum Days
+{
+    Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Moved,
+}
+enum Weather
+{
+    Cold = 5,
+    Normal = 20,
+    Hot = 25,
+    SoHot = 30,
+}
