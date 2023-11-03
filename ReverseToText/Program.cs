d438cf713431bc9c0df11e656a3815c6931f0d35
Tuncer Berk Besne <77729477+Berkbesne@users.noreply.github.com>
2023-11-03 14:45:40 +0300

    Reverse To Text

---
+++ ReverseToText/Program.cs
@@ -0,0 +1,11 @@
+﻿string n=Console.ReadLine();
+string[] a = n.Split(' ');
+
+
+for (int i = 0; i < a.Length; i++)
+{
+    char[] charArray = a[i].ToCharArray();
+    Array.Reverse(charArray);
+    string reversedWord = new string(charArray);
+    Console.WriteLine(reversedWord);
+}
