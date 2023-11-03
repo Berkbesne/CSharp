9e5824074193cb84cb5198ac32563d7ecab41402
Tuncer Berk Besne <77729477+Berkbesne@users.noreply.github.com>
2023-11-03 14:33:00 +0300

    Algorithm Question

---
+++ algorithm_Question/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Please Enter:");
+
+string input = Console.ReadLine();
+string[] parts = input.Split(',');
+if (parts.Length > 0)
+{
+    string result = parts[0]; 
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Hatalı giriş yapıldı.");
+}
