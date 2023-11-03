82c62754ae668d88a66b8602119b14b1f62a832a
Tuncer Berk Besne <77729477+Berkbesne@users.noreply.github.com>
2023-11-03 14:11:50 +0300

    Draw Triangle

---
+++ Draw_Triangle/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Kenar Uzunluğu: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int counter = 1;
+int counter1 = length;
+for (int i = 0; i < length; i++)
+{
+    for (int j = counter1 - 1; j > 0; j--)
+    {
+        Console.Write("");
+    }
+    counter1--;
+   for (int k = 1; k <= counter; k++)
+    {
+        Console.Write("*");
+        Console.Write(" ");
+    }
+    counter++;
+    Console.WriteLine();
+}
